8274825de15917103b42af2725095dd2cdf8fb30
Karen McEwen <karenmcewen.engineer@gmail.com>
2018-06-04 21:02:26 -0400

    first commit

---
+++ pyCalculator.py
@@ -0,0 +1,12 @@
+# this is a simple python calculator that does not use the numpy
+choice = 0
+print('MENU')
+print('1: Add +')
+print('2: Subtract -')
+print('3: Multiply x')
+print('4: Division /')
+print('0: Exit')
+print()
+print()
+choice = int(input('What do you want to do?'))
+print(choice)
