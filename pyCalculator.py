e5f5c127355b83e5e301048027912df277be514a
Karen McEwen <karenmcewen.engineer@gmail.com>
2018-06-04 21:21:28 -0400

    added four operands

--- pyCalculator.py
+++ pyCalculator.py
@@ -8,5 +8,21 @@ print('4: Division /')
 print('0: Exit')
 print()
 print()
-choice = int(input('What do you want to do?'))
+choice: int = int(input('What do you want to do?'))
 print(choice)
+num1 = int(input('enter first number'))
+num2 = int(input('enter second number'))
+if choice == 0:
+    print('ending')
+
+elif choice == 1:
+    print(num1 + num2)
+
+elif choice == 2:
+    print(num1 - num2)
+elif choice == 3:
+    print(num1 * num2)
+elif choice == 4:
+    print(num1 / num2)
+else:
+    print('that is not a valid option')
