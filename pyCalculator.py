cc341edc9581c6a60ee3c25ea557e08f02741322
Karen McEwen <karenmcewen.engineer@gmail.com>
2018-06-09 21:58:00 -0400

    included square and exponent

--- pyCalculator.py
+++ pyCalculator.py
@@ -5,6 +5,8 @@ def menu():
     print('2: Subtract -')
     print('3: Multiply x')
     print('4: Division /')
+    print('5: Square x^2')
+    print('6: Exponent x^y')
     print('Press any other number to exit')
     print()
 
@@ -25,24 +27,37 @@ def dividenums(x,y):
     return x/y
 
 
+def squarenums(x):
+    return x * x
+
+
+def exponentnums(x,y):
+    return x ** y
+
+
 # main program starts here
 choice = 0
 
 while choice >= 0:
     print('Welcome to my simple pyCalculator')
-    num1 = int(input('enter first number'))
-    num2 = int(input('enter second number'))
+    num1 = int(input('Enter first number: '))
+    num2 = int(input('Enter second number: '))
     menu()
     choice: int = int(input('What do you want to do?'))
 
     if choice == 1:
-        print(addnums(num1, num2))
+        print(str(num1) + " + " + str(num2) + " = " + str(addnums(num1, num2)))
+        print()
     elif choice == 2:
         print(subtractnums(num1, num2))
     elif choice == 3:
         print(multiplynums(num1, num2))
     elif choice == 4:
         print(dividenums(num1, num2))
+    elif choice == 5:
+        print(squarenums(num1))
+    elif choice == 6:
+        print(exponentnums(num1, num2))
     else:
         choice = -1
 
