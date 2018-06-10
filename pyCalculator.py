025285382a327a471bbf5c891fec3ae13080d6e5
Karen McEwen <karenmcewen.engineer@gmail.com>
2018-06-09 22:05:42 -0400

    improved output readability

--- pyCalculator.py
+++ pyCalculator.py
@@ -1,4 +1,4 @@
-# this is a simple python calculator that does not use the numpy
+# this is a simple python calculator that does not use numpy
 def menu():
     print('MENU')
     print('1: Add +')
@@ -11,19 +11,19 @@ def menu():
     print()
 
 
-def addnums(x,y):
+def addnums(x, y):
     return x + y
 
 
-def subtractnums(x,y):
+def subtractnums(x, y):
     return x - y
 
 
-def multiplynums(x,y):
+def multiplynums(x, y):
     return x * y
 
 
-def dividenums(x,y):
+def dividenums(x, y):
     return x/y
 
 
@@ -31,7 +31,7 @@ def squarenums(x):
     return x * x
 
 
-def exponentnums(x,y):
+def exponentnums(x, y):
     return x ** y
 
 
@@ -39,27 +39,35 @@ def exponentnums(x,y):
 choice = 0
 
 while choice >= 0:
-    print('Welcome to my simple pyCalculator')
-    num1 = int(input('Enter first number: '))
-    num2 = int(input('Enter second number: '))
+    print('Welcome to my simple pyCalculator!')
+    print()
+    num1 = int(input('Please enter a number: '))
+    num2 = int(input('Enter a second number: '))
     menu()
-    choice: int = int(input('What do you want to do?'))
+    choice: int = int(input('What operation would you like to perform? '))
 
     if choice == 1:
         print(str(num1) + " + " + str(num2) + " = " + str(addnums(num1, num2)))
         print()
     elif choice == 2:
-        print(subtractnums(num1, num2))
+        print(str(num1) + " - " + str(num2) + " = " + str(subtractnums(num1, num2)))
+        print()
     elif choice == 3:
-        print(multiplynums(num1, num2))
+        print(str(num1) + " * " + str(num2) + " = " + str(multiplynums(num1, num2)))
+        print()
     elif choice == 4:
-        print(dividenums(num1, num2))
+        print(str(num1) + " / " + str(num2) + " = " + str(dividenums(num1, num2)))
+        print()
     elif choice == 5:
-        print(squarenums(num1))
+        print(str(num1) + " ^ 2 = " + str(squarenums(num1)))
+        print()
     elif choice == 6:
-        print(exponentnums(num1, num2))
+        print(str(num1) + " ^ " + str(num2) + " = " + str(exponentnums(num1, num2)))
+        print()
     else:
         choice = -1
 
 # exit program
-print('Thank you for using the calculator')
+print()
+print('Thank you for using my pyCalculator!')
+print()
